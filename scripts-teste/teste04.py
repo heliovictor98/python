99d6dfd81126f46ce85f105abe5eef11cb9d4f92
helio.barros <helio.barros@saude.gov.br>
2024-03-25 18:46:50 -0300

    Aula 07

---
+++ scripts-teste/teste04.py
@@ -0,0 +1,7 @@
+print('TESTE DOS NUMEROS')
+n1 = int(input('Primeiro Numero: '))
+n2 = int(input('Segundo Numero: '))
+s = n1 + n2
+#print('A SOMA ENTRE:', n1, ' + ', n2, 'e igual a: ', s)
+print('A SOMA ENTRE {} e {} vale {}'.format(n1,n2,s))
+
