bece0f3208681e86b2d369f311601b0b326cdc53
helio.barros <helio.barros@saude.gov.br>
2024-03-27 13:37:11 -0300

    Calculo de aumento e imposto

--- scripts-exe/exe013.py
+++ scripts-exe/exe013.py
@@ -8,7 +8,8 @@ valorAumento =  (salarioF*vAumento/100)
 valorImposto = (newSalario*vImposto/100)
 valorLiquido = newSalario - (newSalario*vImposto/100)
 
-print('O salario que era de R${}\nCom {}% de aumento, passou a ser de R${}'
+print('O salario que era de R${}'
+      '\nCom {}% de aumento, passou a ser de R${}'
       '\nFoi um aumento de R${}'
       '\nO Imposto de {} seria de R${}'
       '\nO valor liquido seria de R${}'
