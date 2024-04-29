0f275e780a5c6637133d45d2c4ee24b63bd5e327
heliobarros <barros.helio98@gmail.com>
2024-04-28 21:40:46 -0300

    suvida

--- estudo-avancado/main.py
+++ estudo-avancado/main.py
@@ -1,2 +1,11 @@
-msg = 'Olá Mundo!'
-print(msg)
+'''
+Autor: Hélio
+Data: 22/04/2024
+Versão: 1.0
+Descrição: Esse codigo representa os estudos de um desenvolvor
+'''
+
+nome = input('Qual é o seu nome: ')
+idade = input('Qual é a sua idade: ')
+cidade = input('Qual sua cidade')
+print(nome + idade + cidade)
